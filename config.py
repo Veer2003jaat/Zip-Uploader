bdc324a3aa07f01940ccac87c79c6d2bdd873391
Choudhary Veer <jaiveer2003godara@gmail.com>
2025-04-08 16:13:51 +0530

    config.py

--- config.py
+++ config.py
@@ -4,21 +4,21 @@ API_ID = os.environ.get("API_ID", "22581733")
 
 API_HASH = os.environ.get("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7839616256:AAFmadELYRPaku1kHBNW7Kb23VOZ-TiMGLE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7873697236:AAHiUdrogxbCsBDAYTuj3jSZopRzm24dZVQ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 6530997270))
+#OWNER = int(os.environ.get("OWNER", 7414415335))
 
-#LOG = -1002487777733,
+#LOG = -1002442947187,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[6530997270]
-    #for x in (os.environ.get("ADMINS", "6530997270").split()):
+    #ADMINS=[7414415335]
+    #for x in (os.environ.get("ADMINS", "7414415335").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
